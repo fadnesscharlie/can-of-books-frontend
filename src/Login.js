e604136adf672c2c484aa2448e9c4950c3c1b6dc
Charlie <boy_916@hotmail.com>
2021-08-14 17:05:55 -0700

    finished lab 11

--- src/Login.js
+++ src/Login.js
@@ -2,9 +2,11 @@ import React from 'react';
 import 'bootstrap/dist/css/bootstrap.min.css';
 import Card from 'react-bootstrap/Card';
 import './Login.css';
+import LoginButton from './LogInButton.js';
 
 class Login extends React.Component {
   render() {
+    
     return(
       <Card style={{ width: '18rem' }}>
         <Card.Body>
@@ -12,7 +14,8 @@ class Login extends React.Component {
           <Card.Text>
             Click Below to Log In
           </Card.Text>
-          {/* TODO: add a `LoginButton` component here that will log the user in with Auth0 */}
+          {/* DONE: add a `LoginButton` component here that will log the user in with Auth0 */}
+          <LoginButton />
         </Card.Body>
       </Card>
     )
