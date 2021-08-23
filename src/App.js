f48cebcaa0a05b38c220d5170f0045ab5dfe9572
Charlie <boy_916@hotmail.com>
2021-08-22 17:30:10 -0700

    finished lab 13

--- src/App.js
+++ src/App.js
@@ -13,8 +13,6 @@ import { withAuth0 } from '@auth0/auth0-react';
 import Login from './Login.js';
 import axios from 'axios';
 import Button from 'react-bootstrap/Button'
-// import Modal from 'react-bootstrap/Modal'
-import BookForm from './BookForm.js';
 
 class App extends React.Component {
   constructor(props) {
@@ -22,8 +20,6 @@ class App extends React.Component {
     this.state = {
       working: '',
       books: [],
-      show: false,
-      setShow: false,
     }
   }
 
@@ -43,47 +39,6 @@ class App extends React.Component {
     })
   }
 
-  handleCreate = async (bookInfo) => {
-    console.log('bookInfo: ', bookInfo)
-    try {
-      let response = await axios.post(`${process.env.REACT_APP_BACKEND_SERVER}/post-books`, bookInfo);
-      const newBook = response.data;
-      console.log('Data from handle Create Function', newBook)
-      this.setState({
-        // makes sure to save previous books along with new books
-        books: [...this.state.books, newBook],
-      })
-    } catch (err) {
-      console.log(err)
-    }
-  }
-
-  handleDelete = async (id) => {
-    console.log('id', id)
-    try {
-      const { getIdTokenClaims } = this.props.auth0;
-      let tokenClaims = await getIdTokenClaims();
-      const jwt = tokenClaims.__raw;
-      const config = {
-        headers: { "Authorization": `Bearer ${jwt}` },
-      }
-      await axios.delete(`${process.env.REACT_APP_BACKEND_SERVER}/delete-books/${id}`, config)
-      let remainingbooks = this.state.books.filter(book => book._id !== id);
-      this.setState({
-        books: remainingbooks,
-      })
-      console.log(this.state.books)
-    } catch (err) {
-      console.log('Delete function failing: ', err)
-    }
-  }
-
-  handleClose = () => this.setState({ show: false });
-
-  handleShow = () => this.setState({ show: true });
-  // handleShow = () => console.log('handleSHow working');
-
-  updateBooks = () => this.setState({ books: this.state.books});
   render() {
     // Object Destructure
     const { user, isLoading, isAuthenticated } = this.props.auth0;
@@ -106,8 +61,6 @@ class App extends React.Component {
                     <Login />}
 
                   {this.state.working}
-
-                  <BookForm handleCreate={this.handleCreate} handleClose={this.handleClose} handleShow={this.handleShow} show={this.state.show}/>
 
                   {user ? <>
                     <h2>{user.name}</h2>
--- src/BestBooks.js
+++ src/BestBooks.js
@@ -7,6 +7,8 @@ import Carousel from 'react-bootstrap/Carousel'
 import Card from 'react-bootstrap/Card'
 import { withAuth0 } from '@auth0/auth0-react';
 import Books from './Books.js';
+import BookForm from './BookForm.js';
+import Button from 'react-bootstrap/Button'
 
 class MyFavoriteBooks extends React.Component {
   constructor(props) {
@@ -14,31 +16,78 @@ class MyFavoriteBooks extends React.Component {
     this.state = {
       books: [],
       displayBooks: false,
+      show: false,
+      setShow: false,
+      user: {},
     }
   }
 
   componentDidMount = async () => {
   try {
-    // Get your Token for us.
-    // Will learn more in 401
-    const { getIdTokenClaims } = this.props.auth0;
+    // Grabbing Token from Auth0
+    const { getIdTokenClaims, user } = this.props.auth0;
     let tokenClaims = await getIdTokenClaims();
     const jwt = tokenClaims.__raw;
 
     const config = {
       headers: { "Authorization": `Bearer ${jwt}` },
+      // Setting Parameter for email to pass into the backend
+      params: { email: user.email }
     }
     const mongoDBResponse = await axios.get(`${process.env.REACT_APP_BACKEND_SERVER}/books`, config);
 
     this.setState({
       books: mongoDBResponse.data,
+      // updating user to pass into BookForm
+      user: user,
       displayBooks: true,
     })
-    this.props.updateBooks();
   } catch (error) {
     console.log('componentDidMount' , error)
     }
   }
+
+  handleCreate = async (bookInfo) => {
+    console.log('bookInfo: ', bookInfo)
+    try {
+      let response = await axios.post(`${process.env.REACT_APP_BACKEND_SERVER}/post-books`, bookInfo);
+      const newBook = response.data;
+      console.log('Data from handle Create Function: ', newBook)
+      this.setState({
+        // makes sure to save previous books along with new books
+        books: [...this.state.books, bookInfo],
+      })
+    } catch (err) {
+      console.log(err)
+    }
+  }
+
+  handleDelete = async (id) => {
+    console.log('id', id)
+    try {
+      const { getIdTokenClaims, user } = this.props.auth0;
+      let tokenClaims = await getIdTokenClaims();
+      const jwt = tokenClaims.__raw;
+      const config = {
+        headers: { "Authorization": `Bearer ${jwt}` },
+      }
+
+      if (user.email === this.state.books[0].email){
+        await axios.delete(`${process.env.REACT_APP_BACKEND_SERVER}/delete-books/${id}`, config)
+      }
+      let remainingbooks = this.state.books.filter(book => book._id !== id);
+      this.setState({
+        books: remainingbooks,
+      })
+      console.log('Books array',this.state.books)
+    } catch (err) {
+      console.log('Delete function failing: ', err)
+    }
+  }
+
+  handleClose = () => this.setState({ show: false });
+
+  handleShow = () => this.setState({ show: true });
   
   render() {
     let bookRender = this.state.books.map(info => {
@@ -54,6 +103,9 @@ class MyFavoriteBooks extends React.Component {
               <Card.Text>
                 {info.status}
               </Card.Text>
+              <Button 
+              onClick={()=> this.handleDelete(info._id)}
+              >Delete</Button>
             </Card.Body>
           </Card>
         </Carousel.Item>
@@ -68,10 +120,21 @@ class MyFavoriteBooks extends React.Component {
             This is a collection of my favorite books
           </p>
         </Jumbotron>
+        <BookForm 
+        user={this.state.user}
+        handleCreate={this.handleCreate} 
+
+        // Functions
+        handleClose={this.handleClose} 
+        handleShow={this.handleShow} 
+
+        // State
+        show={this.state.show}
+        />
         <Carousel>
           {this.state.displayBooks ? bookRender : ''}
         </Carousel>
-        <Books books={this.state.books} handleDelete={this.props.handleDelete} />
+        {/* <Books books={this.state.books} handleDelete={this.handleDelete} /> */}
       </>
     )
   }
